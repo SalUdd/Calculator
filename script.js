5c2d32b76d31e197022cce9aa619c42afcd234fa
Salah Uddin <81970533+SalUdd@users.noreply.github.com>
2024-10-08 23:40:49 +1100

    added funtion to get data from display, split it and then push to an array. The operand is stored in a variable called dataOperand. Which is later used to determine which calculation to use in the switch case

--- script.js
+++ script.js
@@ -5,12 +5,71 @@
 
 
 const display = document.getElementById("displayText");
+var dataArray = [];
+let dataOperand; 
+
 
 function clr() { 
     document.getElementById("displayText").innerHTML = "";
+    dataArray = [];
+    dataOperand = null;
+    console.log("Cleared display and data");
 } 
 
 
 function appendToDisplay(input) {
     display.innerHTML += input;
 }
+
+function calculateData() {
+    let data = display.innerHTML;
+    console.log("Data before splitting:", data);
+    
+    if (data.indexOf("+") !== -1) {
+        dataOperand = "+";
+        dataArray = data.split(dataOperand);
+    } else if (data.indexOf("-") !== -1) {
+        dataOperand = "-";
+        dataArray = data.split(dataOperand);
+    } else if (data.indexOf("*") !== -1) {
+        dataOperand = "*";
+        dataArray = data.split(dataOperand);
+    } else if (data.indexOf("/") !== -1) {
+        dataOperand = "/";
+        dataArray = data.split(dataOperand);
+    } else {
+        alert("Please select an Operand");
+        return;
+    }
+    
+    dataArray.push(dataOperand);
+    console.log("Data array inside calculateData:", dataArray);
+    console.log("Operand inside calculateData:", dataOperand);
+    
+    let firstNum = parseFloat(dataArray[0]);
+    let secondNum = parseFloat(dataArray[1]);
+    let result;
+    console.log(firstNum);
+    console.log(secondNum);
+    switch (dataOperand) {
+        case "+":
+            result = firstNum + secondNum;
+            break;
+        case "-":
+            result = firstNum - secondNum;
+            break;
+        case "*":
+            result = firstNum * secondNum;
+            break;
+        case "/":
+            result = secondNum !== 0 ? firstNum / secondNum : "Error";
+            break;
+        default:
+            result = "Invalid operation";
+    }
+    
+    display.innerHTML = result;
+    console.log("Calculation result:", result);
+   
+}
+
